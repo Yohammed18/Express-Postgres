e0af43310b3e2dce357f902ea4da78a7ff133796
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-08-26 09:27:56 -0400

    small changes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+var AuthorController = require('./authors/AuthorController')
 
 // middleware to parse JSON bodies
 app.use(express.json())
 
 
-var AuthorController = require('./authors/AuthorController')
+//set the uri 
 app.use('/api', AuthorController);
 
 module.exports = app;
--- authors/AuthorController.js
+++ authors/AuthorController.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var router = express.Router()
-var pool = require('../db')
 var AuthorRepo = require('../authors/AuthorRepo')
 
 
